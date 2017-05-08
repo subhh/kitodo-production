761d351103e0bc907db1b107bc3df7dc121d37a5
Arved Solth <arved.solth@uni-hamburg.de>
2017-05-08 18:00:41 +0200

    Add bean management annotations to forms and update some function names

--- Kitodo/src/main/java/de/sub/goobi/forms/AktuelleSchritteForm.java
+++ Kitodo/src/main/java/de/sub/goobi/forms/AktuelleSchritteForm.java
@@ -39,6 +39,8 @@ import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.locks.ReentrantLock;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletResponse;
 
@@ -79,6 +81,8 @@ import org.kitodo.data.database.persistence.apache.StepObject;
 import org.kitodo.data.elasticsearch.exceptions.CustomResponseException;
 import org.kitodo.services.ServiceManager;
 
+@ManagedBean(eager=true)
+@ViewScoped
 public class AktuelleSchritteForm extends BasisForm {
     private static final long serialVersionUID = 5841566727939692509L;
     private static final Logger logger = LogManager.getLogger(AktuelleSchritteForm.class);
